92ecf71be5b3eca8900df5246693ef6b48a2a002
Ryotaro Ikeda <ryo@longsleeper.com>
2017-12-09 16:04:30 +0900

    Move main logic to train.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from image_container import ImageContainer
-from models import make_network
-from optimizer import make_optimizer
+from train import train
 
 # TODO:
 #   You can change these pathes or make this code to accept
@@ -10,43 +8,12 @@ STYLE_IMAGE_PATH = './images/mona_lisa.jpg'
 NEW_IMAGE_PATH = './images/out3.jpg'
 ITER_EPOCHS = 200
 
-content_img = ImageContainer(CONTENT_IMAGE_PATH)
-style_img = ImageContainer(STYLE_IMAGE_PATH)
-
-# The original paper shows that you can use random noise as an input however,
-# to make learning easier, here, we use exactly the same image as content.
-target_img = ImageContainer(CONTENT_IMAGE_PATH)
-
-net, content_losses, style_losses = make_network(
-  content_img,
-  style_img
+target_image = train(
+  CONTENT_IMAGE_PATH,
+  STYLE_IMAGE_PATH,
+  iter_epochs = 200,
+  content_weight = 1.0,
+  style_weight = 5000.
 )
 
-optimizer = make_optimizer(
-  target_img
-)
-
-i = 0
-while i < ITER_EPOCHS:
-  def closure():
-    global i
-    i += 1
-    # Do the forward pass
-    target_img.clamp_()
-    target_img.feed(net)
-
-    # Do the BP
-    optimizer.zero_grad()
-    total_content_loss = .0
-    total_style_loss = .0
-    for content_loss_layer in content_losses:
-      total_content_loss += content_loss_layer.backward()
-    for style_loss_layer in style_losses:
-      total_style_loss += style_loss_layer.backward()
-
-    total_loss = total_content_loss + total_style_loss
-    if i % 10 == 0:
-      print(f'Iter: {i} / {ITER_EPOCHS}, Loss: {total_loss.data[0]}, C: {total_content_loss.data[0]}, S: {total_style_loss.data[0]}')
-    return total_loss
-  optimizer.step(closure)
-target_img.save(NEW_IMAGE_PATH)
+target_image.save(NEW_IMAGE_PATH)
--- train.py
+++ train.py
@@ -1,6 +1,54 @@
-from image_container import ImageContainer
 from typing import AnyStr
+from image_container import ImageContainer
+from optimizer import make_optimizer
+from models import make_network
 
+class IterationAccumulator(int): pass
 
-def train(content_image_path: AnyStr, style_image_path: AnyStr) -> ImageContainer:
-  pass
+def train(
+  content_image_path: AnyStr,
+  style_image_path: AnyStr,
+  iter_epochs: int = 200,
+  content_weight: float = 1.,
+  style_weight: float = 3000.,
+  ) -> ImageContainer:
+
+  content_image = ImageContainer(content_image_path)
+  style_image = ImageContainer(style_image_path)
+
+  # The original paper shows that you can use random noise as an input however,
+  # to make learning easier, here, we use exactly the same image as content.
+  target_image = ImageContainer(content_image_path)
+
+  net, content_losses, style_losses = make_network(
+    content_image,
+    style_image,
+    content_weight = content_weight,
+    style_weight = style_weight
+  )
+
+  optimizer = make_optimizer(target_image)
+
+  i = IterationAccumulator()
+  while i < iter_epochs:
+    def closure():
+      nonlocal i
+      i += 1
+      # Do the forward pass
+      target_image.clamp_()
+      target_image.feed(net)
+
+      # Do the BP
+      optimizer.zero_grad()
+      total_content_loss = .0
+      total_style_loss = .0
+      for content_loss_layer in content_losses:
+        total_content_loss += content_loss_layer.backward()
+      for style_loss_layer in style_losses:
+        total_style_loss += style_loss_layer.backward()
+
+      total_loss = total_content_loss + total_style_loss
+      if i % 10 == 0:
+        print(f'Iter: {i} / {iter_epochs}, Loss: {total_loss.data[0]}, C: {total_content_loss.data[0]}, S: {total_style_loss.data[0]}')
+      return total_loss
+    optimizer.step(closure)
